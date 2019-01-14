2865a5c60238b44ac067a3dd5a853825730d0b20
Andres Lopez <daztery@gmail.com>
2019-01-13 23:53:18 -0500

    :tada: First commit

---
+++ app/src/main/java/com/example/daztery/agenda/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.daztery.agenda;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText txtNombre,txtDescripcion;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtNombre=(EditText)findViewById(R.id.txtNombre);
+        txtDescripcion=(EditText)findViewById(R.id.txtDescripcion);
+
+
+
+    }
+
+    public void Guardar(View view){
+        String nombre=txtNombre.getText().toString();
+        String datos=txtDescripcion.getText().toString();
+
+        SharedPreferences preferences= getSharedPreferences("agenda",Context.MODE_PRIVATE);
+        SharedPreferences.Editor objEditor=preferences.edit();
+        objEditor.putString(nombre,datos);
+        objEditor.commit();
+
+        Toast.makeText(this,"El contacto ha sido guardado",Toast.LENGTH_LONG).show();
+
+    }
+    public void Buscar(View view){
+
+
+
+    }
+
+}
